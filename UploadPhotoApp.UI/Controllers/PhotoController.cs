f614a9e9b841e21af5c96f3782a2c9f588198e4b
vitaliy.mandzyak <vitaliy.mandzyak@eleks.com>
2018-12-26 15:13:35 +0200

    added uploading files

---
+++ UploadPhotoApp.UI/Controllers/PhotoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UploadPhotoApp.UI.Controllers
+{
+    public class PhotoController : Controller
+    {
+        private readonly IHostingEnvironment _environment;
+
+        public PhotoController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        [HttpPost]
+        public IActionResult Upload(IFormFile file)
+        {
+            var fileExtension = Path.GetExtension(file.FileName);
+
+            var newFileName = $"{ Guid.NewGuid()}{fileExtension}";
+
+            var filePath = Path.Combine(_environment.WebRootPath, "demoImages") + $@"\{newFileName}";
+
+            using (var fs = System.IO.File.Create(filePath))
+            {
+                file.CopyTo(fs);
+                fs.Flush();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- UploadPhotoApp.UI/Startup.cs
+++ UploadPhotoApp.UI/Startup.cs
@@ -5,16 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace UploadPhotoApp.UI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            //services.Configure<MvcOptions>(opt => opt.Filters.Add(new RequireHttpsAttribute()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +38,14 @@ namespace UploadPhotoApp.UI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            //var opt = new RewriteOptions()
+            //    .AddRedirectToHttps();
+
+            //app.UseRewriter(opt);
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute("default", "{controller=Home}/{action=Index}");
             });
         }
     }
